645736b81644409291a7bb46f0ec8928b96c5979
filipParacka <f.paracka@gmail.com>
2022-03-11 17:43:08 +0100

    Day2

--- AdventOfCode/Day2/Program.cs
+++ AdventOfCode/Day2/Program.cs
@@ -1,15 +1,90 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Day2
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            var day2 = new Day2();
+
+            var input = day2.ReadInput();
+
+            var result1 = day2.CalculateDive(input);
+
+            var result2 = day2.CalculateChangedDive(input);
         }
+    }
+
+    public class Day2
+    {
+        public List<Command> ReadInput()
+        {
+            var input = File.ReadAllLines("input.txt");
+
+            var result = input.Select(x => new Command
+            {
+                Position = x.Substring(0, x.LastIndexOf(' ')),
+                Value = int.Parse(x.Substring(x.LastIndexOf(' ') + 1))
+            }).ToList();
+
+            return result;
+        }
+
+        public int CalculateDive(List<Command> commands)
+        {
+            var result = commands.GroupBy(x => x.Position)
+                .Select(c => new
+                {
+                    Position = c.Key,
+                    Value = c.Sum(x => x.Value)
+                });
+
+            var up = result.FirstOrDefault(x => x.Position == "up").Value;
+            var down = result.FirstOrDefault(x => x.Position == "down").Value;
+            var forward = result.FirstOrDefault(x => x.Position == "forward").Value;
+
+            return (down - up) * forward;
+        }
+
+        public int CalculateChangedDive(List<Command> commands)
+        {
+            var aim = 0;
+            var horizontal = 0;
+            var depth = 0;
+
+            foreach (var command in commands)
+            {
+                switch (command.Position)
+                {
+                    case "up":
+                        aim -= command.Value;
+                        break;
+                    case "down":
+                        aim += command.Value;
+                        break;
+                    case "forward":
+                        horizontal += command.Value;
+                        depth += command.Value * aim;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return horizontal * depth;
+        }
+    }
+
+    public class Command
+    {
+        public string Position { get; set; }
+
+        public int Value { get; set; }
     }
 }
--- AdventOfCode/UnitTests/UnitTest1.cs
+++ AdventOfCode/UnitTests/UnitTest1.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Day2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 
 namespace UnitTests
@@ -28,6 +29,56 @@ namespace UnitTests
             var result = day1.GetNumberOfMeasurments(input);
 
             Assert.AreEqual(5,result);
+        }
+
+        [TestMethod]
+        public void Day2Test()
+        {
+            var day2 = new Day2.Day2();
+
+            var commands = new List<Command>();
+
+            commands.Add(new Command()
+            {
+                Position = "forward",
+                Value = 5
+            });
+
+            commands.Add(new Command()
+            {
+                Position = "down",
+                Value = 5
+            });
+
+            commands.Add(new Command()
+            {
+                Position = "forward",
+                Value = 8
+            });
+
+            commands.Add(new Command()
+            {
+                Position = "up",
+                Value = 3
+            });
+
+            commands.Add(new Command()
+            {
+                Position = "down",
+                Value = 8
+            });
+
+            commands.Add(new Command()
+            {
+                Position = "forward",
+                Value = 2
+            });
+
+            var result1 = day2.CalculateDive(commands);
+            var result2 = day2.CalculateChangedDive(commands);
+
+            Assert.AreEqual(150, result1);
+            Assert.AreEqual(900, result2);
         }
     }
 }
